289693b6ba164b43ff3ccd7b15ffbec72cb42889
Nanda Krisbianto <88964291+nandateknik@users.noreply.github.com>
2026-02-15 18:01:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,17 @@
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyTPEvTXoivjyQoLu0upHWmfrlYa_YHB19i2pmD4vE3MnrdV0PAHIEbpxjsDAUw6Vgg/exec';
+// CONFIGURASI NANDA TEKNIK
+const CONFIG = {
+    // Ganti URL iki sakwayah-wayah nek kowe bar Deploy anyar nang Google Apps Script
+    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyTPEvTXoivjyQoLu0upHWmfrlYa_YHB19i2pmD4vE3MnrdV0PAHIEbpxjsDAUw6Vgg/exec',
+    
+    // Koordinat Kantor (Pusat)
+    KANTOR_COORDS: { 
+        lat: -8.1910395, 
+        lng: 114.3697974 
+    },
+    
+    // Settingan Biaya Bensin
+    BENSIN: {
+        FREE_KM: 5,        // Gratis biaya bensin nek kurang saka 5km
+        PER_KM_COST: 10000 // Biaya per km sakwise 5km
+    }
+};
