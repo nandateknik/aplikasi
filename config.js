01185238249d32db555701bd5dbb44766ee21dc1
Nanda Krisbianto <88964291+nandateknik@users.noreply.github.com>
2026-02-16 23:58:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var CONFIG = {
-    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbw0z5YznKUin3ElHIyp4S_kzjU_piLmZlTZA3T8ieTS8KQJ-dah7RMQ0k8vjKDj5Kzk/exec',
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbw0z5YznKUin3ElHIyp4S_kzjU_piLmZlTZA3T8ieTS8KQJ-dah7RMQ0k8vjKDj5Kzk/exec",
     KANTOR_COORDS: {
         lat: -8.1910395,
         lng: 114.3697974
