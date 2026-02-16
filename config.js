886f2787e88359bc22a92cf8c6be844e39efa67a
Nanda Krisbianto <88964291+nandateknik@users.noreply.github.com>
2026-02-16 07:54:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,20 @@
 // CONFIGURASI NANDA TEKNIK
-const CONFIG = {
-    // Ganti URL iki sakwayah-wayah nek kowe bar Deploy anyar nang Google Apps Script
-    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycby7BdaVZzqBYQNaIc8qD2AojHWH-oXY3FzFUSfp8L2Zp83CMK2Q1oihCyPxKvh8Lwza/exec',
-    
-    // Koordinat Kantor (Pusat)
-    KANTOR_COORDS: { 
-        lat: -8.1910395, 
-        lng: 114.3697974 
-    },
-    
-    // Settingan Biaya Bensin
-    BENSIN: {
-        FREE_KM: 5,        // Gratis biaya bensin nek kurang saka 5km
-        PER_KM_COST: 10000 // Biaya per km sakwise 5km
-    }
-};
+// Nganggo pengecekan supaya ora error pas dipanggil bolak-balik
+if (typeof CONFIG === 'undefined') {
+    var CONFIG = {
+        // URL Google Apps Script
+        WEB_APP_URL: 'https://script.google.com/macros/s/AKfycby7BdaVZzqBYQNaIc8qD2AojHWH-oXY3FzFUSfp8L2Zp83CMK2Q1oihCyPxKvh8Lwza/exec',
+        
+        // Koordinat Kantor (Pusat)
+        KANTOR_COORDS: { 
+            lat: -8.1910395, 
+            lng: 114.3697974 
+        },
+        
+        // Settingan Biaya Bensin
+        BENSIN: {
+            FREE_KM: 5,        // Gratis biaya bensin nek kurang saka 5km
+            PER_KM_COST: 10000 // Biaya per km sakwise 5km
+        }
+    };
+}
