aba15593aeb895b738b3395bc391a1410ca9b37c
Nanda Krisbianto <88964291+nandateknik@users.noreply.github.com>
2026-02-16 23:56:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,11 @@
-// CONFIGURASI NANDA TEKNIK - V4.9 CLEAN
-if (typeof CONFIG === 'undefined') {
-    var CONFIG = {
-        // GANTI URL NGISOR IKI NGANGGO URL ANYAR SING MBOK COPY SOKO "NEW DEPLOYMENT"
-        WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbw0z5YznKUin3ElHIyp4S_kzjU_piLmZlTZA3T8ieTS8KQJ-dah7RMQ0k8vjKDj5Kzk/exec',
-        
-        // Koordinat Kantor (Pusat)
-        KANTOR_COORDS: { 
-            lat: -8.1910395, 
-            lng: 114.3697974 
-        },
-        
-        // Settingan Biaya Bensin
-        BENSIN: {
-            FREE_KM: 5,        // Gratis biaya bensin nek kurang saka 5km
-            PER_KM_COST: 10000 // Biaya per km sakwise 5km
-        }
-    };
-}
+var CONFIG = {
+    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbw0z5YznKUin3ElHIyp4S_kzjU_piLmZlTZA3T8ieTS8KQJ-dah7RMQ0k8vjKDj5Kzk/exec',
+    KANTOR_COORDS: {
+        lat: -8.1910395,
+        lng: 114.3697974
+    },
+    BENSIN: {
+        FREE_KM: 5,
+        PER_KM_COST: 10000
+    }
+};
