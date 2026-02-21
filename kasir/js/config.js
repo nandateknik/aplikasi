b2834b9a116d0ffff85d80297065d0c6da270d57
Nanda Krisbianto <88964291+nandateknik@users.noreply.github.com>
2026-02-21 21:24:18 +0700

    Update config.js

--- kasir/js/config.js
+++ kasir/js/config.js
@@ -8,7 +8,7 @@ db.version(1).stores({
 const URL_API = 'https://script.google.com/macros/s/AKfycbyDo9YNJ-z_T51k8gtJc8lxl8LUft1OED2Sm5ExkT6RA-SOvuP3WRHr9Yd2v2LRlZ7e/exec';
 
 // Fungsi proteksi halaman
-const session = JSON.parse(localStorage.getItem("pos_session"));
-if (!session && !window.location.href.includes('login.html')) {
-    window.location.href = 'login.html';
-}
+// const session = JSON.parse(localStorage.getItem("pos_session"));
+// if (!session && !window.location.href.includes('login.html')) {
+//     window.location.href = 'login.html';
+// }
