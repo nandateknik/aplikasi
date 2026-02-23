2d9dba3f620753323ba8a43cc91fa63237cd974b
Nanda Krisbianto <88964291+nandateknik@users.noreply.github.com>
2026-02-24 05:49:39 +0700

    Update config.js

--- adminv2/js/config.js
+++ adminv2/js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    GAS_URL: "https://script.google.com/macros/s/AKfycbw8IvxxDseel160nSjS9qDmT5NSAnKFGXQks7kE4Lq-kTtUXITrjwIIv-v-iJ_pXHabLQ/exec",
+    GAS_URL: "https://script.google.com/macros/s/AKfycby0ID6l9b1t3LJU4zDp9HFinH5YOA9gBnbaqMFSUwLVOYcu3SVXbR5LGYM96ogpU0-mHg/exec",
     APP_NAME: "Servis Pro Dashboard"
 };
